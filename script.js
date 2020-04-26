ca8a8a6c5e9f8ef5ad97f025c0a48b735b4db762
Hassan Javaid <hassanjrao@gmail.com>
2020-04-27 02:44:15 +0500

    final commit

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 
 
+$(document).ready(function(){
+
+    $("a.fa").hover(
+        
+    function(){
+        alert("you hover fa ");
+    },
+    function(){
+        alert("Bye bye");
+    }
+
+);
+
+});
+
+
+
+
 let picsArr = ["https://cdn.pixabay.com/photo/2017/08/27/10/16/interior-2685518_960_720.jpg", "https://cdn.pixabay.com/photo/2017/09/20/11/53/architecture-2768284_960_720.jpg", "https://cdn.pixabay.com/photo/2018/01/26/08/15/dining-room-3108037_960_720.jpg", "https://cdn.pixabay.com/photo/2016/11/22/21/46/arches-1850730_960_720.jpg", "https://cdn.pixabay.com/photo/2016/01/19/17/02/spiral-1149509_960_720.jpg", "https://cdn.pixabay.com/photo/2016/08/12/20/43/stained-glass-1589648_960_720.jpg"];
 
 let imgTag = document.getElementById("header-img-1");
